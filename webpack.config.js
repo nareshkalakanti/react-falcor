9f8e5ba293adabf9dfbcc38fca02842bd97f673f
Naresh K <naresh.x.kalakanti@aib.ie>
2018-01-02 15:04:51 +0000

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,14 @@
+module.exports= {
+
+    entry: './index.js',
+    output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+   module: {
+    loaders: [
+      { test: /\.js$/,loader: "babel" }
+    ]
+  },
+
+};
